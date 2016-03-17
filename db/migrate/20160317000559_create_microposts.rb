216d65d8ef58344090f4f58e0f08a6b7f883dcf6
codeamac08 <codeamac08@gmail.com>
2016-03-16 18:09:22 -0600

    Commit de Micropost add

--- db/migrate/20160317000559_create_microposts.rb
+++ db/migrate/20160317000559_create_microposts.rb
@@ -1,8 +1,9 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
-      t.string :content
-      t.belongs_to :user, index: true
+      t.text :content
+      t.integer :user_id
+
       t.timestamps null: false
     end
   end
